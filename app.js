b0cd865ad08500ddf55a3d24165b4716342522c6
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-17 11:26:31 -0600

    updated routes for nodemailer

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 const queries = require('./queries')
 const bodyParser = require('body-parser')
 const database = require('./database-connection')
+const nodemailer = require('nodemailer')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/', (request, response) => {
     queries.list('nanny_account_info').catch(console.error)
@@ -60,5 +63,49 @@ app.get('/', (request, response) => {
   app.use((request, response) => {
     response.sendStatus(404)
   })
+
+  //Nodemailer
+
+  app.post('/api/form', (request, response) => {
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact Details</h3>
+            <ul>
+                <li>Name: ${request.body.user_name}</li>
+                <li>Email: ${request.body.user_email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${request.body.user_message}</p>
+        `
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'ssvw2yzwjvwdryyb@ethereal.email',
+                pass: 'xNHuNGjBa4k6HcB48Z'
+            }
+        })
+
+        let mailOptions = {
+            from: 'test@testaccount.com',
+            to: 'ssvw2yzwjvwdryyb@ethereal.email',
+            replyTo: 'test@testaccount.com',
+            subject: 'new message',
+            text: request.body.user_message,
+            html: htmlEmail    
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log('Message sent: %s', info.message)
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+        })
+    })
+})
+
+
 
 module.exports = app
